5014bec5806d27673246b874ae6b555652b124e0
Tarek Meftah <sudtechnics@gmail.com>
2019-10-02 21:53:15 +0200

    change layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-card class="mx-auto mt-4">
-      <v-card-title>How old am I?</v-card-title>
-      <v-card-text>
-        <v-text-field label="please give your name" />
-      </v-card-text>
-      <v-date-picker
-        v-model="picker"
-        color="deep-orange accent-2"
-
-        :type="'date'"
-      />
-      <v-card-actions>
-        <v-spacer />
-        <v-btn outlined fab color="deep-orange lighten-1">
-          save
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-layout row>
+    <v-flex fill-height d-flex>
+      <v-card>
+        <v-card-title>How old am I?</v-card-title>
+        <v-card-text>
+          <v-text-field label="please give your name" />
+        </v-card-text>
+        <v-date-picker
+          v-model="picker"
+          color="deep-orange accent-2"
+          full-width
+          :type="'date'"
+        />
+        <v-card-actions>
+          <v-btn
+            color="deep-orange lighten-1"
+          >
+            save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 <script>
