ec042135c6d4415354a7c3e1748881e12b3a9207
Tarek Meftah <sudtechnics@gmail.com>
2019-10-04 17:51:30 +0200

    add vuex store

---
+++ store/index.js
@@ -0,0 +1,50 @@
+
+export const state = () => ({
+  birthday: new Date().toISOString().substr(0, 10),
+  currentUserName: '',
+  list: [],
+  timer: null
+})
+
+export const mutations = {
+  setBirthday (state, birthday) {
+    state.birthday = birthday
+  },
+  setUserName (state, username) {
+    state.currentUserName = username
+  },
+  addUser (state) {
+    state.list.push({
+      username: state.currentUserName,
+      birthday: state.birthday,
+      age: null
+    })
+  },
+
+  updateAge (state) {
+    state.list.forEach((element) => {
+      const birthday = this.$moment(element.birthday)
+      const years = this.$moment().diff(birthday, 'years')
+      birthday.add(years, 'years')
+      const months = this.$moment().diff(birthday, 'months')
+      birthday.add(months, 'months')
+      const days = this.$moment().diff(birthday, 'days')
+
+      element.age = {
+        years,
+        months,
+        days
+      }
+    })
+  }
+}
+
+export const actions = {
+  startTimer ({ commit }) {
+    setInterval(() => commit('updateAge'), 1000)
+  }
+}
+
+export const getters = {
+  users: (state) => { return state.list }
+}
