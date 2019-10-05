859ecd7fafd3d4846f8ba834c638ca32b9ceb0f0
Tarek Meftah <sudtechnics@gmail.com>
2019-10-05 21:23:56 +0200

    change layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-app>
-    <v-content app>
-      <v-container>
-        <nuxt />
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <nuxt />
+          </v-col>
+        </v-row>
       </v-container>
     </v-content>
     <v-footer app>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,80 @@
 <template>
-  <v-row>
-    <v-col
-      sm="8"
-      offset-sm="2"
-      md="6"
-      offset-md="3"
+  <v-card class="elevation-12">
+    <v-toolbar
+      color="deep-orange accent-3"
+      dark
+      flat
     >
-      <v-flex fill-height>
-        <v-card>
-          <v-card-title>How old am I?</v-card-title>
-          <v-card-text>
-            <v-text-field
-              v-model="setUsername"
-              label="please give your name"
-            />
-          </v-card-text>
-          <v-date-picker
-            v-model="birthday"
-            color="deep-orange accent-2"
-            full-width
-            :max="new Date().toISOString().substr(0, 10)"
-            :type="'date'"
-            :locale="getLocale"
-          />
-          <v-card-actions>
-            <div class="flex-grow-1" />
-            <v-btn
-              color="green dark-1"
-              small
-              fab
-              @click="addUser"
-            >
-              <v-icon>mdi-content-save</v-icon>
-            </v-btn>
-          </v-card-actions>
-          <hr>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th class="text-left">
-                    Name
-                  </th>
-                  <th class="text-left">
-                    Birthday
-                  </th>
-                  <th class="text-left">
-                    Age
-                  </th>
-                  <th />
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  v-for="item in getUsers"
-                  :key="item.name"
-                >
-                  <td>{{ item.username }}</td>
-                  <td>{{ item.birthday }}</td>
-                  <td>{{ item.age.years }} year(s) , {{ item.age.months }} month(s), {{ item.age.days }} day(s)</td>
-                  <td>
-                    <v-btn
-                      color="red"
-                      x-small
-                      fab
-                      @click="removeUser(item)"
-                    >
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                  </td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-card>
-      </v-flex>
-    </v-col>
-  </v-row>
+      <v-toolbar-title>How old am I?</v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text>
+      <v-text-field
+        v-model="setUsername"
+        label="please give your name"
+        type="text"
+        solo
+      />
+    </v-card-text>
+    <v-date-picker
+      v-model="birthday"
+      color="deep-orange accent-1"
+      full-width
+      :max="new Date().toISOString().substr(0, 10)"
+      :type="'date'"
+      :locale="getLocale"
+    />
+    <v-card-actions>
+      <div class="flex-grow-1" />
+      <v-btn
+        color="green dark-1"
+        small
+        fab
+        @click="addUser"
+      >
+        <v-icon>mdi-content-save</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <hr>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Birthday
+            </th>
+            <th class="text-left">
+              Age
+            </th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in getUsers"
+            :key="item.name"
+          >
+            <td>{{ item.username }}</td>
+            <td>{{ item.birthday }}</td>
+            <td>{{ item.age.years }} year(s) , {{ item.age.months }} month(s), {{ item.age.days }} day(s)</td>
+            <td>
+              <v-btn
+                color="red"
+                x-small
+                fab
+                @click="removeUser(item)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-card>
 </template>
 <script>
 export default {
